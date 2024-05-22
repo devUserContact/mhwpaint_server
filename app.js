9efdc9e63e14f1aee6231d8000877e3c73c30084
devUserContact <developer.user.contact@protonmail.com>
2024-05-22 12:26:49 -0700

    added `127.0.0.1` to `listen`

--- app.js
+++ app.js
@@ -2,4 +2,4 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => res.send('this is an action test'))
-app.listen(3000, () => console.log('Server ready'))
+app.listen(3000, '127.0.0.1', () => console.log('Server ready'))
