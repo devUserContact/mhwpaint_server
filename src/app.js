36d9caad59e8dba44b6aa3d1ef93caf515a6aff0
devUserContact <developer.user.contact@protonmail.com>
2024-05-27 23:56:22 -0700

    added `$id` route

--- src/app.js
+++ src/app.js
@@ -1,19 +1,24 @@
-var express = require("express");
-var cors = require("cors");
-var db = require("./db.js");
+var express = require('express')
+var cors = require('cors')
+var db = require('./db.js')
 
-const app = express();
+const app = express()
 
 var corsOptions = {
-  origin: "http://localhost:3001",
+  origin: 'https://mhwpaint.com',
   optionsSuccessStatus: 200,
-};
+}
 
-app.get("/", cors(corsOptions), (req, res) => res.send("test"));
+app.get('/', cors(corsOptions), (req, res) => res.send('test'))
 
-app.get("/mhwpaint/gallery", cors(corsOptions), async function (req, res) {
-  let gallery = await db.connectToServer();
-  res.send(gallery);
-});
+app.get('/mhwpaint/gallery', cors(corsOptions), async function (req, res) {
+  let gallery = await db.queryGallery()
+  res.send(gallery)
+})
 
-app.listen(3000, () => console.log("Server ready"));
+app.get('/mhwpaint/gallery/:id', cors(corsOptions), async function (req, res) {
+  let work = await db.queryWork(req.params.id)
+  res.send(work)
+})
+
+app.listen(3000, () => console.log('Server ready'))
--- src/db.js
+++ src/db.js
@@ -1,7 +1,7 @@
 const mariadb = require('mariadb')
 require('dotenv').config()
 
-exports.connectToServer = async function () {
+exports.queryGallery = async function () {
   let conn, query
   try {
     conn = await mariadb.createConnection({
@@ -22,16 +22,27 @@ exports.connectToServer = async function () {
   }
 }
 
-//exports.connectToDb = async function () {
-//  console.log("test");
-//  let conn;
-//  try {
-//    conn = await pool.getConnection();
-//    const res = await conn.query("SHOW DATABASES;");
-//    console.log(res);
-//  } catch (error) {
-//    console.error(error);
-//  } finally {
-//    conn.end();
-//  }
-//};
+exports.queryWork = async function (id) {
+  let conn, query
+  try {
+    conn = await mariadb.createConnection({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PSSWRD,
+      socketPath: process.env.DB_SOCK,
+    })
+
+    console.log('connected ! connection id is ' + conn.threadId)
+    let unique_id = id
+    console.log(unique_id)
+    await conn.query('USE mhwpaint;')
+    query = await conn.query(
+      `SELECT * FROM works WHERE unique_id = "${unique_id}";`,
+    )
+  } catch (err) {
+    console.debug('not connected due to error: ' + err)
+  } finally {
+    conn.close()
+    return query
+  }
+}
